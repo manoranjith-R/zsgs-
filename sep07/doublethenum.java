d693637b00b70232ba298388a86ad7706c97200a
manoranjith-R <112952808+manoranjith-R@users.noreply.github.com>
2022-09-07 17:47:53 +0530

    Add files via upload

---
+++ sep07/doublethenum.java
@@ -0,0 +1,22 @@
+package sep07;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class doublethenum {
+
+	public static void main(String[] args) {
+		Scanner input =new Scanner(System.in);
+		int num=input.nextInt();
+		System.out.println("enter the position");
+		int pos=input.nextInt();
+		int ans=num;
+		for(int i=1;i<pos;i++) {
+			ans+=num;
+			num=ans;
+		}
+		System.out.println(ans);
+	}
+	}
+
+
